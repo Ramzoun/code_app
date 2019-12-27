d68fd82b1346836ac7fb06a42070620bc48c0869
ramzoun <info.bouaziz.ramzi@gmail.com>
2019-12-27 11:19:49 +0000

    'last03'

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -1,62 +1,54 @@
-$(document).ready(function(){      
-    'use strict'	
-			
-	if($('#meLite').length){
-		console.log("app 1");
-		//const urlParams = new URLSearchParams(window.location.search);
-		//var p = "http://192.168.1.222:8080/category/"+urlParams.get('th')+"/"+urlParams.get('s');
+$(document).ready(function () {
+	'use strict'
+
+	//Demo Purposes.
+	$('a').on('click', function () { var attrs = $(this).attr('href'); if (attrs === '#') { return false; } });
+
+	//FastClick
+	$(function () { FastClick.attach(document.body); });
+
+	//Preload Image
+	$(function () { $(".preload-image").lazyload({ threshold: 500 }); });
+
+
+	var owl = $('.owl-carousel');
+	
+	owl.owlCarousel({
+		margin: 10,
+		nav: true,
+		dots:false,
+		loop: false,
+		lazyLoad: true,
+		autoplay: false,
+		items: 1
+	});
+
+
+
+	$(function () {
 		var p = "http://51.178.25.226:8080/demo";
-		//console.log(p)
-		
-		$.getJSON(p, function(data) {     
-			// Remove all child nodes (including text nodes) 
-			$("#meLite").empty();      
-			$.each(data, function(i, field){
-			$("#meLite").append('<div>'
-							+ '<img width="700" class="owl-lazy" src="http://51.178.25.226:8080/res/205">'
-							//+ '<div class="above-overlay cover-content-bottom center-text">'
-							+  '<h4>'+field.qText+'</h4>'
-							+ 		'<p>'+field.pro1+'<br>'+field.pro2+'<br>'+field.pro3+'</p>'
-							//+		'<p class="color-gray-light bottom-0">'
-							//+			'A professional mobile template, fast loading, fluidly animated mobile user experience for you.'
-							//+		'</p>'
-							//+'</div>'
-							//+'<div class="overlay overlay-gradient"></div>'
-						+'</div>');
-			});      
-		}).fail(function(jqxhr, textStatus, error){
-									
-									var err = textStatus + ", " + error;
-									console.log( "Request Failed: " + err );
-									
-									window.location = "pageapp-login.html";
-								});
-								
-		//console.log('____s___');
-		
-}
 
-	
-	function init_template(){	
+		$.getJSON(p, function (data) {
+
+			$.each(data, function (i, field) {
+				owl.trigger('add.owl.carousel','<img width="700" class="preload-image" src="http://51.178.25.226:8080/res/205">'
+					+ '<h4>' + field.qText 
+					+ '</h4>'
+					+ '<p>' + field.pro1 + '<br>' + field.pro2 + '<br>' + field.pro3 + '</p>'
+					+ '</div>');
+			});
+
+			//owl.trigger('to.owl.carousel',0);
+			owl.trigger('refresh.owl.carousel');
 			
-		//Demo Purposes.
-		$('a').on('click', function(){var attrs = $(this).attr('href');	if(attrs === '#'){return false;}});
+		}).fail(function (jqxhr, textStatus, error) {
 
-        //FastClick
-        $(function() {FastClick.attach(document.body);});
-                
-        //Preload Image
-        $(function() {$(".preload-image").lazyload({threshold : 500});});
-        
+			var err = textStatus + ", " + error;
+			console.log("Request Failed: " + err);
 
-		// aaaa
-		
-		
+			window.location = "pageapp-login.html";
+		});
+	});
 
-
-		
-	}
-    
-	
 });
 
